7970de486221bf1293eab1fc2254354d8cf301dd
rqdmap <55649208+rqdmap@users.noreply.github.com>
2023-06-20 20:56:33 +0800

    更新主题基本框架, 实现TOC监控, 代码块复制等功能

---
+++ static/js/hit.js
@@ -0,0 +1,92 @@
+(function(){
+	function getTimestamp() {
+		var timestamp = Date.parse(new Date());
+		return timestamp;
+	}
+
+	function getPageID() {
+		return document.getElementsByTagName('meta')['id'].content
+	}
+
+	function getPlatform() {
+		var plat = navigator.platform;
+		var valid = ["Win", "Linux", "Mac", "Android", "iPhone", "iPad"]
+		for(var i in valid){
+			if (plat.toLowerCase().indexOf(valid[i].toLowerCase()) != -1) {
+				return valid[i]
+			}
+		}
+		return "Others"
+	}
+
+	function getBrowser() {
+		var ua = navigator.userAgent;
+		if (ua.indexOf("Maxthon") != -1) {
+			return "Maxthon";
+		} else if (ua.indexOf("MSIE") != -1) {
+			return "MSIE";
+		} else if (ua.indexOf("Firefox") != -1) {
+			return "Firefox";
+		} else if (ua.indexOf("Chrome") != -1) {
+			return "Chrome";
+		} else if (ua.indexOf("Opera") != -1) {
+			return "Opera";
+		} else if (ua.indexOf("Safari") != -1) {
+			return "Safari";
+		} else {
+			return "Others";
+		}
+	}
+
+	function getBrowserLanguage() {
+		var lang = navigator.language;
+		return lang != null && lang.length > 0 ? lang : "";
+	}
+
+	function hit(){
+		var pageID	= encodeURIComponent(getPageID());
+		var browser = encodeURIComponent(getBrowser());
+		var lang	= encodeURIComponent(getBrowserLanguage());
+		var plat	= encodeURIComponent(getPlatform());
+
+		var request = new XMLHttpRequest();
+		request.open('GET', 'https://api.rqdmap.top/hit?id=' + pageID + '&browser=' + browser + '&lang=' + lang + '&plat=' + plat, true);
+		request.send();
+
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 &&
+				(request.status == 200 || request.status == 304)) {
+				console.log(request.responseText);
+			}
+		}
+	}
+
+	function query(){
+		var views = document.getElementsById("post-views")[0];
+		var totalViews = document.getElementsById("total-views")[0];
+
+		var request = new XMLHttpRequest();
+		var pageID	= encodeURIComponent(getPageID());
+
+		request.open('GET', 'https://api.rqdmap.top/hitcount?id=' + pageID, true);
+		request.send();
+
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 &&
+				(request.status == 200 || request.status == 304)) {
+				res = request.responseText.split(' ');
+
+				if(res == null){
+					res = [-1,-1];
+				}
+				if(views != null) views.innerText += res[0].trim();
+				totalViews.append(" | Total views: " + res[1].trim());
+
+				hit();
+			}
+		}
+	}
+
+	if(document.location.host.includes('localhost')) return ; 
+	query();
+})();
---
+++ static/js/toc_inspect.js
@@ -0,0 +1,49 @@
+(function(){
+
+	const selector =  ['h2', 'h3', 'h4']
+
+
+	const content = document.querySelector('article');
+
+	const all_toc = content.querySelectorAll(selector.join());
+	const toc = document.querySelector('.toc');
+
+
+	const throttle = function(func, wait){
+		let timeoutId;
+		return function() {
+			if (!timeoutId) {
+				clearTimeout(timeoutId);
+				timeoutId = setTimeout(() => {
+					timeoutId = null;
+					func.apply(this, arguments);
+				}, wait);
+			}
+		}
+	}
+
+	const scrollHandler = function(){
+		var now = null;
+		for (var i = all_toc.length - 1; i >= 0; i--){
+			if(all_toc[i].offsetTop <= window.scrollY + 5){
+				now = all_toc[i];
+				break;
+			}
+		}
+
+		if(now == null) now = all_toc[0];
+
+		for(let i of toc.getElementsByTagName('a')){
+			console.log(i.text, now.innerText);
+			if(i.text == now.innerText){
+				i.classList.add('active');
+			}
+			else{
+				i.classList.remove('active');
+			}
+		}
+	}
+
+	window.addEventListener('scroll', throttle(scrollHandler, 500));
+
+})()
